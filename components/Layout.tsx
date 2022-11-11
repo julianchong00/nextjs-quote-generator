2b9657921579c2aa214ead0ee370fa0a277b9e79
Julian Chong <julianchong00@outlook.com>
2022-11-11 11:26:20 +1100

    style(eslint/prettier): setup eslint and prettier in project

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,18 @@
 import Head from "next/head";
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 export default function Layout({ children }: Props) {
-    return (
-        <>
-            <Head>
-                <title>Quote Generator</title>
-                <meta name="description" content="Quote Generator Web App" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className="flex flex-grow h-screen">{children}</main>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Quote Generator</title>
+        <meta name="description" content="Quote Generator Web App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-grow h-screen">{children}</main>
+    </>
+  );
 }
--- components/QuotePage.tsx
+++ components/QuotePage.tsx
@@ -3,29 +3,34 @@ import QuoteDisplay from "./QuoteDisplay";
 import { QuoteData } from "./types/types";
 
 export default function QuotePage() {
-    const [quote, setQuote] = useState({} as QuoteData);
+  const [quote, setQuote] = useState({} as QuoteData);
 
-    const callQuoteAPI = async () => {
-        fetch("https://api.quotable.io/random?maxLength=170")
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                const quoteData: QuoteData = data;
-                setQuote(quoteData);
-            });
-    };
+  const callQuoteAPI = async () => {
+    fetch("https://api.quotable.io/random?maxLength=170")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const quoteData: QuoteData = data;
+        setQuote(quoteData);
+      });
+  };
 
-    useEffect(() => {
-        console.log("QutoeDisplay useEffect called");
-    }, []);
+  useEffect(() => {
+    console.log("QutoeDisplay useEffect called");
+  }, []);
 
-    return (
-        <div className="flex flex-col justify-center items-center w-screen">
-            <div className="py-3">
-                <button className="border-solid rounded-md bg-slate-500 hover:bg-slate-700 p-2" onClick={callQuoteAPI}>Click Me</button>
-            </div>
-            <QuoteDisplay {...quote} />
-        </div>
-    )
+  return (
+    <div className="flex flex-col justify-center items-center w-screen">
+      <div className="py-3">
+        <button
+          className="border-solid rounded-md bg-slate-500 hover:bg-slate-700 p-2"
+          onClick={callQuoteAPI}
+        >
+          Click Me
+        </button>
+      </div>
+      <QuoteDisplay {...quote} />
+    </div>
+  );
 }
