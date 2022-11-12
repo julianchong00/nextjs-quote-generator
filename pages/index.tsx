78224fcd1a6ca29cd448dbb7f0589f3aaedf0468
Julian Chong <julianchong00@outlook.com>
2022-11-12 17:15:30 +1100

    feat(footer): added a footer to page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,14 @@ import QuotePage from "../components/QuotePage";
 
 export default function Index() {
   return (
-    <div className="flex flex-col justify-center items-center w-screen">
+    <div className="h-screen w-screen">
       <QuotePage />
+
+      <div className="flex justify-center">
+        <footer className="text-center text-xs text-slate-500">
+          <p>Julian Chong 2022</p>
+        </footer>
+      </div>
     </div>
   );
 }
